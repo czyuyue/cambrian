9bfac5749c8793b59da4e257ee9820ba27b4cf8e
czyuyue <yuyue__yuyue@outlook.com>
2025-10-03 18:07:04 +0000

    numworkers

--- scripts/cambrian/pretrain_vnorm.sh
+++ scripts/cambrian/pretrain_vnorm.sh
@@ -50,7 +50,7 @@ python cambrian/train/train_tpu.py \
     --tf32 False \
     --model_max_length 2048 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 0 \
+    --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to wandb \
     --run_name $CKPT_NAME \
